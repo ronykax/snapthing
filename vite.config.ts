c683c07672f2c7190226001cc7fdab544b8ca7a1
Rony Kati <ronykax@gmail.com>
2026-07-17 17:27:16 +0530

    remove pwa stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      manifest: {
-        description: "Take a picture, add text, and save to Drive!",
-        icons: [
-          {
-            sizes: "192x192",
-            src: "/vite.svg",
-            type: "image/svg+xml",
-          },
-        ],
-        name: "SnapThing",
-        short_name: "SnapThing",
-        theme_color: "#000000",
-      },
-      registerType: "autoUpdate",
-    }),
   ],
 });
